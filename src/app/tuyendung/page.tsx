ad76af3325fac273e276083ec22fafa4041355b0
quocb <quocbao180702@gmail.com>
2024-04-15 12:45:35 +0700

    giao dien tuyendung + about us

--- src/app/tuyendung/page.tsx
+++ src/app/tuyendung/page.tsx
@@ -1,5 +1,5 @@
 import BreadCrum from "@/components/breadcrumb/breadcrumb";
-import { Col, Container, Row } from "react-bootstrap";
+import { Badge, Col, Container, Row } from "react-bootstrap";
 
 function TuyenDung() {
     const title = "Tuyển Dụng"
@@ -7,14 +7,54 @@ function TuyenDung() {
         <>
             <BreadCrum title={title} />
             <Container>
-                <Row className="">
-                    <Col style={{height: "500px"}} className="w-100 d-flex justify-content-center">
-                            <div className="tuyendung">
-                                <h3 className="text-uppercase">Kế Toan</h3>
-                            </div>
-                    </Col>
+                <Row className="mt-2">
+                    <div className="d-flex justify-content-between" style={{ padding: "20px", borderRadius: "5px", border: "1px solid black" }}>
+                        <h1>Kế Toán</h1>
+                        <div>
+                            <Badge bg="danger" style={{ fontSize: "20px" }}>Đang Cần Gấp</Badge>
+                        </div>
+                    </div>
                 </Row>
-                <Row>pagination</Row>
+                <Row className="mt-2">
+                    <div className="d-flex justify-content-between" style={{ padding: "20px", borderRadius: "5px", border: "1px solid black" }}>
+                        <h1>Kế Toán</h1>
+                        <div>
+                            <Badge bg="success" style={{ fontSize: "20px" }}>Đang Cần Gấp</Badge>
+                        </div>
+                    </div>
+                </Row>
+                <Row className="mt-2">
+                    <div className="d-flex justify-content-between" style={{ padding: "20px", borderRadius: "5px", border: "1px solid black" }}>
+                        <h1>Kế Toán</h1>
+                        <div>
+                            <Badge bg="success" style={{ fontSize: "20px" }}>Đang Cần Gấp</Badge>
+                        </div>
+                    </div>
+                </Row>
+                <Row className="mt-2">
+                    <div className="d-flex justify-content-between" style={{ padding: "20px", borderRadius: "5px", border: "1px solid black" }}>
+                        <h1>Kế Toán</h1>
+                        <div>
+                            <Badge bg="success" style={{ fontSize: "20px" }}>Đang Cần Gấp</Badge>
+                        </div>
+                    </div>
+                </Row>
+                <Row className="mt-2">
+                    <div className="d-flex justify-content-between" style={{ padding: "20px", borderRadius: "5px", border: "1px solid black" }}>
+                        <h1>Kế Toán</h1>
+                        <div>
+                            <Badge bg="success" style={{ fontSize: "20px" }}>Đang Cần Gấp</Badge>
+                        </div>
+                    </div>
+                </Row>
+                <Row className="mt-2">
+                    <div className="d-flex justify-content-between" style={{ padding: "20px", borderRadius: "5px", border: "1px solid black" }}>
+                        <h1>Kế Toán</h1>
+                        <div>
+                            <Badge bg="success" style={{ fontSize: "20px" }}>Đang Cần Gấp</Badge>
+                        </div>
+                    </div>
+                </Row>
             </Container>
         </>
     );
--- src/app/vechungtoi/page.tsx
+++ src/app/vechungtoi/page.tsx
@@ -1,14 +1,75 @@
 "use client"
 import BreadCrum from "@/components/breadcrumb/breadcrumb";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 function VeChungToi() {
     const title = "Về Chúng Tôi"
     return (
         <>
             <BreadCrum title={title} />
-            <Container style={{height: "800px"}}>
-
+            <Container>
+                <Row style={{ marginTop: "40px" }}>
+                    <Col className="mr-4" md={6}>
+                        <div className="d-flex">
+                            <div style={{ marginTop: "30px", height: "300px", borderRight: "2px solid black" }}>
+                            </div>
+                            <div style={{ marginTop: "30px", width: "100px", height: "1px", border: "1px solid black" }}></div>
+                            <div className="d-flex justify-content-center align-items-center" style={{ backgroundColor: "#ee7621", width: "100px", maxHeight: "100px", border: "2px solid black" }}>
+                                <div style={{ padding: "5px", fontSize: "48px", color: "#fff" }}>
+                                    1
+                                </div>
+                            </div>
+                            <div style={{ padding: "10px" }}>
+                                <h4>Kinh Nghiệm</h4>
+                                <div>
+                                    <p>Với bác sĩ có nhiều năm kinh nghiệm</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col className="mr-4" style={{ marginTop: "200px" }} md={6}>
+                        <div className="d-flex">
+                            <div className="text-right" style={{ padding: "10px" }}>
+                                <h4>Kinh Nghiệm</h4>
+                                <div>
+                                    <p>Với bác sĩ có nhiều năm kinh nghiệm</p>
+                                </div>
+                            </div>
+                            <div className="d-flex justify-content-center align-items-center" style={{ backgroundColor: "#ee7621", width: "100px", maxHeight: "100px", border: "2px solid black" }}>
+                                <div style={{ padding: "5px", fontSize: "48px", color: "#fff" }}>
+                                    2
+                                </div>
+                            </div>
+                            <div style={{ marginTop: "30px", width: "100px", height: "1px", border: "1px solid black" }}></div>
+                            <div style={{ marginTop: "30px", height: "300px", borderRight: "2px solid black" }}>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                {/* <Row style={{marginTop: "40px"}}>
+                    <Col md={6}></Col>
+                    
+                </Row> */}
+                <Row style={{ marginTop: "40px" }}>
+                    <Col className="mr-4" md={6}>
+                        <div className="d-flex">
+                            <div style={{ marginTop: "30px", height: "300px", borderRight: "2px solid black" }}>
+                            </div>
+                            <div style={{ marginTop: "30px", width: "100px", height: "1px", border: "1px solid black" }}></div>
+                            <div className="d-flex justify-content-center align-items-center" style={{ backgroundColor: "#ee7621", width: "100px", maxHeight: "100px", border: "2px solid black" }}>
+                                <div style={{ padding: "5px", fontSize: "48px", color: "#fff" }}>
+                                    1
+                                </div>
+                            </div>
+                            <div style={{ padding: "10px" }}>
+                                <h4>Kinh Nghiệm</h4>
+                                <div>
+                                    <p>Với bác sĩ có nhiều năm kinh nghiệm</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
             </Container>
         </>
     );
